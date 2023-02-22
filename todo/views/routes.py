3a0a26da44606d5682990b838934731467ba6067
Yashvardhan Batra <yashvardhanbatra410@gmail.com>
2023-02-22 13:38:06 +1000

    commit

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -10,7 +10,7 @@ def get_todos():
      "id": 1,
      "title": "Watch CSSE6400 Lecture",
      "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-     "completed": False,
+     "completed": True,
      "deadline_at": "2023-02-27T00:00:00",
      "created_at": "2023-02-20T00:00:00",
      "updated_at": "2023-02-20T00:00:00"
@@ -22,7 +22,7 @@ def get_todo(id):
      "id": id,
      "title": "Watch CSSE6400 Lecture",
      "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-     "completed": False,
+     "completed": True,
      "deadline_at": "2023-02-27T00:00:00",
      "created_at": "2023-02-20T00:00:00",
      "updated_at": "2023-02-20T00:00:00"
@@ -34,7 +34,7 @@ def create_todo():
      "id": 1,
      "title": "Watch CSSE6400 Lecture",
      "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-     "completed": False,
+     "completed": True,
      "deadline_at": "2023-02-27T00:00:00",
      "created_at": "2023-02-20T00:00:00",
      "updated_at": "2023-02-20T00:00:00"
@@ -46,7 +46,7 @@ def update_todo(id):
      "id": id,
      "title": "Watch CSSE6400 Lecture",
      "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-     "completed": False,
+     "completed": True,
      "deadline_at": "2023-02-27T00:00:00",
      "created_at": "2023-02-20T00:00:00",
      "updated_at": "2023-02-20T00:00:00"
@@ -58,7 +58,7 @@ def delete_todo(id):
      "id": id,
      "title": "Watch CSSE6400 Lecture",
      "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-     "completed": False,
+     "completed": True,
      "deadline_at": "2023-02-27T00:00:00",
      "created_at": "2023-02-20T00:00:00",
      "updated_at": "2023-02-20T00:00:00"
